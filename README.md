6d5420d3bad61f113896810d0d87fc4ab794c10f
iLexGit <alex.mr.1020@gmail.com>
2022-02-21 18:56:10 +0000

    Show regex matching attack

--- README.md
+++ README.md
@@ -21,4 +21,4 @@
 - [ ] Allow RegEx testing
 - [ ] Ask if output to file rather than work on CLI
 - [ ] Add more stats
-- [ ] Replace menu with flag options and implement *-h*
+- [x] Replace menu with flag options and implement *-h*
--- logParser.py
+++ logParser.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import re
 attacks = []
 attackers = []
 
@@ -15,55 +16,48 @@ class Attack:
         self.logs = logs
 
 def main():
-    file = json.load(loadAttacks())
     
-    for entry in file:
-        attacks.append(Attack(entry["sourceip"], entry["destinationip"], entry["logs"]))
+    if sys.argv[1] == "-A":
+        file = json.load(loadAttacks())
+        
+        for entry in file:
+            attacks.append(Attack(entry["sourceip"], entry["destinationip"], entry["logs"]))
 
-    for attack in attacks:
+        for attack in attacks:
             index = isUnique(attack.src)
             if index == 0:
                 attackers.append(Attacker(attack.src, 1))
             else:
                 attackers[index].reqs+=1
+        
+        if sys.argv[3] == "-l":
+            listAttackers()
+        
+        elif (sys.argv[3] == "-s") and (len(sys.argv) > 4):
+            showAttack(sys.argv[4])
+        
+        elif (sys.argv[3]) == "-r" and (len(sys.argv) > 4):
+            for attack in attacks:
+                regexAttack(attack, sys.argv[4])
 
-    if sys.argv[2] == "-h":
-        printHelp()
-
-    elif sys.argv[2] == "-l":
-        listAttackers()
-    
-    elif sys.argv[2] == "-s":
-        if len(sys.argv) == 3:
-            showAttack(sys.argv[2])
         else:
-            print("[KO] Bad use\n")
             printHelp()
-
-    #while True:
-        #Menu()
-
-def loadAttacks ():
-    try:
-        file = open(sys.argv[1], "r")
-    except IOError:
-        print("File: <", sys.argv[1], "> not found")
-        sys.exit()
-    print("\n[OK] Logs loaded")
-    return file
-
-def Menu():
-    op = input("-----------------------------------------\n\t1. Show Attackers\n\t2. Show Attack from IP\n\te. Exit\n\toption -> ")
-    if op == "1":
-        print("\nAttackers:\n")
-        #listAttackers()
-    elif op == "2":
-        showAttack()
-    elif op == "e":
-        print("\nEXITING...\n")
-        sys.exit(1)
+    
+    elif sys.argv[1] == "-L":
+        print("TODO - implement regular log parser")
+    
     else:
-        print("\tInvalid Input")
+        printHelp()
+    
+
+def loadAttacks():
+    try:
+        file = open(sys.argv[2], "r")
+    except IOError:
+        print("File: <", sys.argv[2], "> not found")
+        sys.exit()
+    print("\n[OK] Logs loaded\n")
+    return file
 
 def isUnique(src):
     index = 0
@@ -80,19 +74,37 @@ def listAttackers():
 
 def showAttack(ip):
     index=0
-    for attack in attacks:
-        if attack.src == ip:
-            index+=1
-            print("\nATTACK ", index, "\n\tTarget: ", attack.dst, "\n\tLogs ", len(attack.logs),":")
-            for i in range(len(attack.logs)):
-                print("\t\t", attack.logs[i]["time"], "\t[", attack.logs[i]["parsedMessage"]["level"], "]", "\n\t", attack.logs[i]["parsedMessage"]["message"][attack.logs[i]["parsedMessage"]["message"].index("\""):], "\n")
+    if len(sys.argv) > 6 and sys.argv[5] == "-r":
+        for attack in attacks:
+            if attack.src == ip:
+                regexAttack(attack, sys.argv[6])
+
+    else:
+        for attack in attacks:
+            if attack.src == ip:
+                index+=1
+                print("\nATTACK ", index, "\n\tTarget: ", attack.dst, "\n\tLogs ", len(attack.logs),":")
+                for i in range(len(attack.logs)):
+                    print("\t\t", attack.logs[i]["time"], "\t[", attack.logs[i]["parsedMessage"]["level"], "]", "\n\t", attack.logs[i]["parsedMessage"]["message"][attack.logs[i]["parsedMessage"]["message"].index("\""):], "\n")
+
+def regexAttack(attack, regex):
+    x = "-"
+    for i in range(len(attack.logs)):
+        x = (re.findall(regex, attack.logs[i]["parsedMessage"]["message"][attack.logs[i]["parsedMessage"]["message"].index("\""):]))
+        if len(x) > 0:
+            print("\nSource", attack.src, "->", attack.dst)
+            for p in range(len(attack.logs)):
+                print("\t", attack.logs[p]["parsedMessage"]["message"][attack.logs[p]["parsedMessage"]["message"].index("\""):])
+            break
 
 def printHelp():
     print("Log parser v1\nAlejandro Marti, Lupovis\nhttps://github.com/iLexGit/Attack-Log-parser\n")
-    print("Usage: python logParser.py [LOG_FILE] [OPTIONS]\n")
-    print("\t-h\t\tShow help ouptut")
-    print("\t-l\t\tList source IP with number of requests sent")
+    print("Usage: python logParser.py [TYPE OF LOG] <LOG_FILE_PATH> [OPTIONS]\n")
+    print("[TYPE OF LOG]\n\t-A\t\tAttacks log\n\t-L\t\tRegular log\n")
+    print("[ATTACK LOG OPTIONS]\n")
+    print("\t-l\t\tList source IP with number of attacks performed")
     print("\t-s <IP>\t\tPrint all logs from specified source IP")
+    print("\t-r <RegEx> \tPrint matching logs with regular expression")
 
 if __name__ == "__main__":
     main()
